d18513f9a951373b810fb707f425027fa5104417
Murhussain <husseinmurashi@yahoo.com>
2023-03-21 17:34:56 +0200

    Preventing usr to click btn with no amount

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ new Vue({
   data: {
     currencies: {},
     name: 'mur',
-    amount: null,
+    amount: 0,
     from: 'EUR',
-    to: 'USD'
+    to: 'USD',
+    result: 0
   },
   mounted() {
     this.getCurrencies();
@@ -13,6 +14,14 @@ new Vue({
   computed:{
     formattedCurrencies() {
       return Object.values(this.currencies);
+    },
+
+    calculateResult() {
+      return (Number(this.amount) * this.result).toFixed(3);
+    },
+
+    disabled() {
+      return this.amount === 0 || !this.amount;
     }
   },
   methods: {
@@ -30,6 +39,15 @@ new Vue({
         this.currencies = response.data.results;
         localStorage.setItem('currencies', JSON.stringify(response.data.results));
       });
+    },
+
+    convertCurrency() {
+      const key = `${this.from}_${this.to}`;
+      axios.get(`https://free.currencyconverterapi.com/api/v6/convert?q=${key}`)
+      .then((response) => {
+        console.log(response)
+        this.result = response.data.result[key].val;
+      })
     }
   }
 })
