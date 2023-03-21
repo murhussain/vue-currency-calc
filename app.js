dfe1671cafcbfe524276b2b3e4e21b7378861f56
Murhussain <husseinmurashi@yahoo.com>
2023-03-21 15:49:09 +0200

    Making http request to api

--- app.js
+++ app.js
@@ -4,9 +4,23 @@ new Vue({
     currencies: {}
   },
   mounted() {
-    axios.get('https://free.currencyconverterapi.com/api/v6/currencies')
+    this.getCurrencies();
+  },
+  methods: {
+    getCurrencies() { 
+
+      // saving data on local storage to avoid application to make more request
+      const currencies = localStorage.getItem('currencies')
+      if (currencies) {
+        this.currencies = JSON.parse(currencies);
+        return;
+      }
+
+      axios.get('https://free.currencyconverterapi.com/api/v6/currencies')
       .then(response => { 
         this.currencies = response.data.results;
+        localStorage.setItem('currencies', JSON.stringify(response.data.results));
       });
+    }
   }
 })
